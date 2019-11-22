0307b02554f35af5bc1299f8700ca955fbb904d5
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-11-21 21:12:00 -0500

    20191121 Another attempt at commit

--- First_Msg.py
+++ First_Msg.py
@@ -2,11 +2,7 @@
 message = "Doo wah diddy dom doh diddy doo. Parker is lying on the couch."
 print (message)
 
-<<<<<<< HEAD
 message="If Parker bops Penny on the head one more time, she's gonna make him sorry."
-=======
-message="If Parker bops Penny on the head, one more time, she's gonna make him sorry."
->>>>>>> 2a6d5162ec55437b8b6a999c939a5fe42ef98954
 print(message)
 
 message=" "
@@ -14,9 +10,8 @@ print(message)
 
 message="Fiona Hill is pretty badass"
 print (message)
-<<<<<<< HEAD
 
 message=" "
 print(message)
-=======
->>>>>>> 2a6d5162ec55437b8b6a999c939a5fe42ef98954
+
+print("Just a test")
