09d711b0a1c730da724aadbe9db308a45fdcef26
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-11-30 01:31:08 -0500

    20191120 Finally got quotation marks to work

---
+++ famous_quote.py
@@ -0,0 +1,10 @@
+#20191129
+#Chapter 2: Famous Quote
+#Start Program
+print(" ")
+
+print(""""Don't cry because it's over, smile because it happened." Dr. Seuss""")
+print("""Dr. Seuss once said "Don't cry because it's over, smile because it happened." """)
+
+#End Program 
+print(" ")
---
+++ famous_quote2.py
@@ -0,0 +1,15 @@
+#20191129
+#Chapter 2: Famous Quote
+#Start Program
+print(" ")
+
+famous_name = "Dr. Seuss"
+quote = """"Don't cry because it's over, smile because it happened." """
+
+print (famous_name)
+print(quote)
+
+print(f"My favorite quote is by {famous_name}. He once said {quote}")
+
+#End Program 
+print(" ")
