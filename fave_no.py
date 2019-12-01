0bb0c23220575628945dc347f36180656bb59f91
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-01 16:48:58 -0500

    20191201 Added extra exercises

---
+++ fave_no.py
@@ -0,0 +1,11 @@
+#20191201
+#Chapter 2: Favorite Number
+
+#Start Program
+print(" ")
+
+FAVE_NO = 100
+print(f"My favorite number of all time is {FAVE_NO}.")
+
+#End Program
+print(" ")
---
+++ number_eight.py
@@ -0,0 +1,24 @@
+#20191201
+#Chapter 2: Number Eight
+
+#Start Program
+print(" ")
+
+print("6 + 2 =")
+print(6+2)
+print(" ")
+
+print("4 * 2 =")
+print(4*2)
+print(" ")
+
+print("2 ** 2 =")
+print(2**2)
+print(" ")
+
+print("10 - 2 =")
+print(10-2)
+print(" ")
+
+#End Program
+print(" ")
