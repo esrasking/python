188decac34ca7bcdeba4566545f07f0382b8f68a
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-02 14:13:50 -0500

    20191202 Adding cars.py

---
+++ Chapter 3/cars.py
@@ -0,0 +1,36 @@
+#20191202 
+#Chapter 3: Organizing a List
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+
+#Permanently sorting a list in ascending order
+cars = ['bmw','audi','toyota','subaru']
+print(cars)
+cars.sort()
+print(cars)
+print('')
+
+#Permanently sorting a list in descending order
+cars = ['bmw','audi','toyota','subaru']
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+#Sorting the list in reverse ORDER permanently, not reverse ALPHABETICALLY
+print("")
+cars = ['bmw','audi','toyota','subaru']
+print(cars)
+cars.reverse()
+print(cars)
+
+#Finding the length of a list
+print('')
+cars = ['bmw','audi','toyota','subaru']
+print (cars)
+len(cars)
+print(len(cars))
+
+
+#End Program
+print (f"\nEND PROGRAM\n")
