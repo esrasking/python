946bb1ef35267d08e0a0117f0a83e55bbe205abf
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-02 17:10:15 -0500

    20191202 Added dinner guests

---
+++ Chapter 3/ch3_dinner_guests.py
@@ -0,0 +1,15 @@
+#20191202 
+#Chapter 3.9: Dinner Guests
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+
+#Determining the number of guests being invited
+print(f"Original List\n\t")
+guests=['dave matthews','dr. seuss','robin williams']
+print(guests)
+print(f"\nThe number of guests is: ")
+print(len(guests))
+
+#End Program
+print(f"\nEND PROGRAM\n")
