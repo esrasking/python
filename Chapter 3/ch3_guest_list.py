e01d20ad852db1f57b8bc70d57a3e2c8b12b89fc
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-02 00:17:02 -0500

    20191202 Adding exercise 3.4 Guest List

--- Chapter 3/ch3_guest_list.py
+++ Chapter 3/ch3_guest_list.py
@@ -6,7 +6,12 @@ print('')
 print("START PROGRAM")
 print('')
 
-
+guests=['dave matthews','albert einstein','robin williams']
+print(f"{guests}\n")
+print(f"These are the guests I'd like to individually invite to dinner:\n\t{guests[0].title()}, \n\t{guests[1].title()}, \n\t{guests[2].title()}.\n")
+print(f"Dear {guests[0].title()}, we would like to cordially invite you to our next roundtable dinner. Please RSVP by Friday.\n")
+print(f"Dear {guests[1].title()}, we would like to cordially invite you to our next roundtable dinner. Please RSVP by Friday.\n")
+print(f"Dear {guests[2].title()}, we would like to cordially invite you to our next roundtable dinner. Please RSVP by Friday.\n")
 
 #End Program
 print('')
