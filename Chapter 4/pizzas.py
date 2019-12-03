9a0ec52b02d5e93bda051c445e216395e87e9ff9
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-03 14:47:40 -0500

    20191202 Added pizzas.py

---
+++ Chapter 4/pizzas.py
@@ -0,0 +1,15 @@
+#20191202 
+#Chapter 4.1: Pizzas.
+
+#Start Program
+print (f"\nSTART PROGRAM\n")
+
+pizzas = ['diavolo','gourmet','peperoni']
+for pizza in pizzas:
+    print(pizza)
+    print(f"I think {pizza} is very healthy.")
+    
+print (f"\nI hope you enjoy pizza as much as i do. \n")
+
+#End Program
+print(f"\nEND PROGRAM\n")
