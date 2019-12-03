bf4494e8321241bc8dfd1e84c928a9ef6eabbfc5
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-03 14:58:49 -0500

    20191203 Added animals.py

---
+++ Chapter 4/animals.py
@@ -0,0 +1,15 @@
+#20191203
+#Chapter 4.2: Animals
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+
+animals = ['cat','dog','horse']
+
+for animal in animals:
+    print(f"I think {animal}s make good pets.\n")
+
+print(f"Do you notice that all these animals are mammals?")
+
+#End Program
+print(f"\nEND PROGRAM\n")
