21074c3b8af1c2da9b20935965551a43543fc50f
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-12 15:20:01 -0500

    201212 Added toppings.py

---
+++ Chapter 5/toppings.py
@@ -0,0 +1,22 @@
+#20191212
+#Chapter 5
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+
+requested_toppings = ['mushrooms','extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print(f"\nFinished making your pizza!\n")
+
+#When running a single block of code, use if-elif
+#When running multiple blocks of code, use multiple if statements.
+
+#End Program
+print(f"\nEND PROGRAM\n")
