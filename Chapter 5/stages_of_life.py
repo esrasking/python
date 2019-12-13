d878b4ef9d890cd6b7a3417cee9038e3f50df6aa
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-12 23:35:32 -0500

    20191212 Added stages_of_life

---
+++ Chapter 5/stages_of_life.py
@@ -0,0 +1,24 @@
+#20191212
+#Chapter 5: Exercise 5.6
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+
+age = 14
+
+if age < 2:
+    print("Oh, you're such a cute baby!")
+elif age >=2 and age < 4:
+    print("You're not toddling, you're stright up steppin'.")
+elif age >= 4 and age <13:
+    print("You're the coolest kid on the block.")
+elif age >=13 and age <20:
+    print("Angsty much? You're not the first human to hit puberty.")
+elif age >= 20 and age <65:
+    print("Adulting is hard. Maybe that's why you have a headache.") 
+else:
+    print("Being older means you don't have to give a shit what people think about you.")
+
+
+#End Program 
+print(f"\nEND PROGRAM\n")
