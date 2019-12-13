99536c2464f6476ed6d3a210bb18442f154370fb
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-13 16:39:07 -0500

    20191213 Added hello_admin_v2.py

---
+++ Chapter 5/hello_admin_v2.py
@@ -0,0 +1,22 @@
+#20191213
+#Chapter 5: Exercise 5.8 Hello Admin
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+
+print(f"------------------------------\n")
+
+
+user_names = ['ana','david','jason','liz','georgia','admin']
+
+for user in user_names:
+    if user == 'admin':
+        print(f"Hello {user}, would you like a status report?")
+    else:
+        print(f"Hello {user.title()}, thank you for logging in today.")
+
+print(f"\n------------------------------")
+
+
+#End Program
+print(f"\nEND PROGRAM\n")
