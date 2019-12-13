6d144c0bd48b9559ec65f7504a307926089f089b
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-12 23:51:58 -0500

    20191212 Added favorite_fruit.py

---
+++ Chapter 5/favorite_fruit.py
@@ -0,0 +1,26 @@
+#20191212
+#Chapter 5: Exercise 5.7 Favorite Fruit
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+
+favorite_fruits = ['lemons','limes','strawberries']
+
+if 'bananas' in favorite_fruits:
+    print(f"You really like bananas!")
+if 'lemons' in favorite_fruits:
+    print(f"You really like lemons!")
+if 'coconuts' in favorite_fruits:
+    print("You really like coconuts!")
+if 'strawberries' in favorite_fruits:
+    print("You really like strawberries!")
+if 'limes' in favorite_fruits:
+    print("You really like limes!")
+
+print(f"\nDid you notice you got enough fruit to make a cocktail?")
+print(f"Just sayin...")
+
+
+
+#End Program
+print(f"\nEND PROGRAM\n")
