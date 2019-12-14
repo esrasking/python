9898f2235a4bb52af9b8052534e484e6f308302c
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-14 00:22:42 -0500

    20191214 Added beginning code for alien.py

---
+++ Chapter 5/alien.py
@@ -0,0 +1,31 @@
+#20191213
+#Chapter 6: Dictionaries
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+print(f"------------------------------\n")
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+print(alien_0['points'])
+print(f"------------------------------")
+
+
+#Assessing the values in a Dictionary
+new_points = alien_0['points']
+print(f"Well done! You've just earned {new_points} points!")
+print(f"------------------------------")
+
+#Setting x and y coordinates
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
+print(alien_0)
+print(f"------------------------------")
+
+#Modifying values in a Dictionary
+
+print(f"------------------------------")
+
+#End Program
+print(f"\nEND PROGRAM\n")
