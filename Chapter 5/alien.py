7759388a7d809d1d0c534ee0874ab1bd47d30863
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-17 17:40:28 -0500

    20191217 Modified alien dictionary

--- Chapter 5/alien.py
+++ Chapter 5/alien.py
@@ -24,8 +24,39 @@ print(alien_0)
 print(f"------------------------------")
 
 #Modifying values in a Dictionary
+alien_0 = {'color':'green'}
+print(f"The alien is {alien_0['color']}.")
 
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}.")
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original position: {alien_0['x_position']}")
+
+#Move the alien to the right.
+#Determine how far to move the alien based on its current speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    #This must be a fast alien.
+    x_increment = 3
+
+#The new position is the old position plus the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f"New position: {alien_0['x_position']}")
 print(f"------------------------------")
+
+#Removing Key-Value Pairs
+#Using the del statement deletes the key-value pair permanently.
+alien_0 = {'color': 'green', 'points':5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
+print(f"------------------------------")
+
 
 #End Program
 print(f"\nEND PROGRAM\n")
