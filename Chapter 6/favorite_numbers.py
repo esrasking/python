e1fccabd3dfe1e2036ef800ac980cda97823fc3d
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-18 00:23:09 -0500

    20191218 Added favorite_numbers.py

---
+++ Chapter 6/favorite_numbers.py
@@ -0,0 +1,22 @@
+#20191218
+#Chapter 6: Exercise 6.2 Favorite Numbers
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+print(f"------------------------------")
+
+favorite_numbers = {
+    'jason': 4,
+    'liz': 2,
+    'ana zoe': 13,
+    'dj': 10,
+    'oma': 5
+}
+
+print(favorite_numbers)
+favorite_number = favorite_numbers['ana zoe']
+print(f"Ana Zoe's favorite number is {favorite_number}.")
+
+print(f"------------------------------")
+#End Program
+print(f"\nEND PROGRAM\n")
