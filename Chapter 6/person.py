0bdc53190bb107d683b9c5bc22c1222c56391765
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-17 23:45:01 -0500

    20191217 Added exercise 6.1

---
+++ Chapter 6/person.py
@@ -0,0 +1,17 @@
+#20191217
+#Chapter 6: Exercise 6.1 Person
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+print(f"------------------------------")
+
+#Storing the information of someone I know in a dictionary.
+person = {'first_name': 'georgia','last_name': 'blackwell','age': 40,'city':'frankfurt'}
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
+
+print(f"------------------------------")
+#End Program
+print(f"\nEND PROGRAM\n")
