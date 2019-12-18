9e9c973146a70d9d135f00875c56c5b2fd1ba066
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-17 22:47:59 -0500

    20191217 Added favorite_languages.py

---
+++ Chapter 6/favorite_languages.py
@@ -0,0 +1,20 @@
+#20191217
+#Chapter 6: Favorite Languages
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+print(f"------------------------------")
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'C',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}.")
+
+#End Program 
+print(f"------------------------------")
+print(f"\nEND PROGRAM\n")
