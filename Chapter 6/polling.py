57beb93dbb94273f672328f16a7e7583edb1c008
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2019-12-19 23:53:27 -0500

    20191219 Added polling.py

---
+++ Chapter 6/polling.py
@@ -0,0 +1,22 @@
+#20191219
+#Chapter 6: Exercise 6.6 Polling
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+print(f"------------------------------")
+
+users = ['jen','sarah','edward','phil','jason','karen','liz']
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for user in users:
+    
+
+print(f"------------------------------")
+#End Program
+print(f"\nEND PROGRAM\n")
