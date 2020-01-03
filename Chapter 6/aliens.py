fd0e9537de9c0bf096f6f50ea259a3e0a50898cd
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2020-01-03 17:25:01 -0500

    20200103 Made modifications

--- Chapter 6/aliens.py
+++ Chapter 6/aliens.py
@@ -14,7 +14,36 @@ aliens = [alien_0, alien_1, alien_2]
 
 for alien in aliens:
     print(alien)
+print(f"------------------------------")
 
+#Making an empty list to store aliens
+aliens = []
+
+#Make aliens green
+
+for alien in range(30):
+    new_alien = {'color':'green','points':5,'speed':'slow'}
+    aliens.append(new_alien)
+
+#Now changing the properties for three of the aliens
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+
+#Show the first 5 aliens
+
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+
+#Show how many aliens were created
+print(f"The total number of aliens generated is {len(aliens)}.")
 
 print(f"------------------------------")
 #End Program
