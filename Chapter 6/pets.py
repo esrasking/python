c024fa06fecf56b7793bface0855278ab5f726d3
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2020-01-05 02:47:39 -0500

    20200105 Created pets.py

---
+++ Chapter 6/pets.py
@@ -0,0 +1,15 @@
+#20200105
+#Chapter 6: Exercise 6.8 Pets
+#Making several dictionaries where each represents a different pet.
+#Store the dictionaries in a list called Pets.
+#Loop through the list and print all the info for each pet.
+
+#START PROGRAM
+print(f"\nSTART PROGRAM")
+print("--------------------\n")
+
+
+
+print("--------------------\n")
+#END PROGRAM
+print(f"END PROGRAM")
