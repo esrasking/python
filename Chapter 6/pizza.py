cae95f1947d451c827ebde58669fa984c278e3c6
Elizabeth Rasking <lizhome@Elizabeths-MacBook-Pro-2.local>
2020-01-04 19:59:40 -0500

    20200103 Added pizza.py

---
+++ Chapter 6/pizza.py
@@ -0,0 +1,28 @@
+#20200103
+#Chapter 6
+
+#Start Program
+print(f"\nSTART PROGRAM\n")
+print(f"------------------------------")
+
+
+#Storing information about a pizza being ordered.
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms','extra cheese']
+}
+
+#Summarize the order
+print(f"You ordered a {pizza['crust']}-crust pizza "
+    "with the following toppings:")
+
+for topping in pizza['toppings']:
+    print(f"\t" + topping)
+
+#IMPORTANT: When you have a long string that you need to break up, choose 
+#a point to break the line, indent, and then continue with an open 
+#quotation mark. The ouput will still print on one line.
+
+print(f"------------------------------")
+#End Program
+print(f"\nEND PROGRAM\n")
