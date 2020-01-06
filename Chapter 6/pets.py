3ca07d2f00c3e44ff1a859c93fc8bce0d9f2614e
Elizabeth Rasking <lizhome@elizabethsmbp2.lan>
2020-01-06 01:57:02 -0500

    20200106 Completed pets.py assmt

--- Chapter 6/pets.py
+++ Chapter 6/pets.py
@@ -9,8 +9,46 @@
 print(f"\nSTART PROGRAM")
 print("--------------------\n")
 
+#Here are my three separate pet dictionaries
+
+pet_1 = {
+    'animal' : 'cat',
+    'pet_name' : 'parker',
+    'owner' : 'liz rasking',
+    'location' : 'ellicott city',
+}
+
+pet_2 = {
+    'animal' : 'dog',
+    'pet_name' : 'tiki',
+    'owner' : 'carol young',
+    'location' : 'orlando',
+    }
+
+pet_3 = {
+    'animal' : 'rat',
+    'pet_name' : 'squeakers',
+    'owner' : 'anna white',
+    'location' : 'sykesville',
+}
+
+#Here is my list storing the dictionaries
+
+pets = [pet_1, pet_2, pet_3]
+
+#Printing raw data in dictionary
+for pet in pets:
+    print (pet)
+
+print("\n--------------------")
+
+#Printing formatted data from dictionaries
+for pet in pets:
+    for a,b in pet.items():
+        print(f"{a.title()} : {b.title()}")
+    print(" ")
 
 
-print("--------------------\n")
+print("\n--------------------")
 #END PROGRAM
 print(f"END PROGRAM")
